7121faf1186ee7233860f8df32f8eabfedebd80f
Wadi1998 <you@example.com>
2025-10-26 16:53:31 +0100

    hh

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+﻿import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+  { path: 'dashboard', loadChildren: () => import('./features/dashboard/dashboard.routes').then(m => m.DASHBOARD_ROUTES) },
+  { path: 'clients', loadChildren: () => import('./features/clients/clients.routes').then(m => m.CLIENTS_ROUTES) },
+  { path: 'rendez-vous', loadChildren: () => import('./features/appointments/appointments.routes').then(m => m.APPOINTMENTS_ROUTES) },
+  { path: '**', redirectTo: 'dashboard' }
+];
